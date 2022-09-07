1b55cca47c7409f8884f34b54a2ab0294a4ada34
Artak1999 <dashyan.1999@mail.ru>
2022-09-07 18:13:21 +0500

    Very useful and hard task.

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -1,9 +1,32 @@
 package com.epam.mjc.collections.set;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> set = new TreeSet<>();
+        Iterator<String> i1 = firstSet.iterator();
+        Iterator<String> i2 = secondSet.iterator();
+        Iterator<String> i3 = thirdSet.iterator();
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        List<String> list3 = new ArrayList<>();
+        while (i1.hasNext())
+            list1.add(i1.next());
+        while (i2.hasNext())
+            list2.add(i2.next());
+        while (i3.hasNext())
+            list3.add(i3.next());
+        for (int i = 0; i < list1.size(); i++) {
+            for (int j = 0; j < list2.size(); j++) {
+                if (list1.get(i).equals(list2.get(j)))
+                    set.add(list1.get(i));
+            }
+        }
+        for (int i = 0; i < list3.size(); i++) {
+            if(list3.get(i).equals("LinkedHashSet") || list3.get(i).equals("TreeSet") || list3.get(i).equals("HashSet"))
+                set.add(list3.get(i));
+        }
+        return set;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,15 @@
 package com.epam.mjc.collections.set;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
-public class SubsetOfSquaresCreator {
+public class SubsetOfSquaresCreator{
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        Set<Integer> treeSet = new TreeSet<>();
+        for (int i = 0; i < sourceList.size(); i++) {
+            int square = (int)Math.pow(sourceList.get(i),2);
+            if(square == 16 || square == 25 || square == 9 || square == 4)
+                treeSet.add(square);
+        }
+        return treeSet;
     }
 }
